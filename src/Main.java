c9f7d66b8f7615465169dd6b864e2dbfb711c228
alexe <aerneher@myuax.com>
2026-03-04 10:21:26 +0100

    feat(Main): Creacion de metodos necesarios

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        // Crear Concesionario con capacidad para 12 vehiculos
+        Concesionario concesionario = new Concesionario(12);
+
+        // Crear entrenadores
+        vehiculo vehiculo1 = new vehiculo(1, "8829MWC", "Bmw", 32900.99);
+        vehiculo vehiculo2 = new vehiculo(2, "9642HDP", "Audi", 22450.33);
+        vehiculo vehiculo3 = new vehiculo(3, "0027KLK", "Seat", 12000.99);
+
+        // Registrar al menos 3 entrenadores
+        concesionario.addVehiculo(vehiculo1);
+        concesionario.addVehiculo(vehiculo2);
+        concesionario.addVehiculo(vehiculo3);
+
+        // Mostrar entrenadores
+        System.out.println("LISTADO DE VEHICULOS:");
+        concesionario.obtainVehiculos();
+
+        // Mostrar total almacenado
+        System.out.println("\nTotal de vehiculos almacenados: "
+                + concesionario.());
+
+        // Probar búsqueda existente
+        System.out.println("\nBúsqueda vehiculo con ID 1:");
+        System.out.println(concesionario.findVehiculo(1));
+
+        // Probar búsqueda inexistente
+        System.out.println("\nBúsqueda vehiculo con ID 99:");
+        System.out.println(concesionario.findVehiculo(99));
     }
 }
